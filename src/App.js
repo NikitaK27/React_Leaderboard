f6da518f47a253132de29b9f1747931253329570
NikitaK27 <80617331+NikitaK27@users.noreply.github.com>
2022-01-08 23:15:35 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import  { Table } from './Table'
+import { useState } from 'react';
+
+
+function App() {
+  return (
+    <>
+    <div className="App">
+        <Table/>
+    </div>
+    </>
+  );
+}
+
+export default App;
